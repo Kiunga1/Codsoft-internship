0c1fd1c00554cbae157719b7781fbbc69a434289
ann5306 <annkiunga@gmail.com>
2024-03-04 09:19:28 +0300

    First commit

---
+++ Bookmark-Landing-Page/js/script.js
@@ -0,0 +1,8 @@
+function showOverlay() {
+    document.getElementById('overlay').style.display = 'flex';
+    document.getElementById('overlay').classList.add('Slide-from-right');
+}
+
+function hideOverlay() {
+    document.getElementById('overlay').style.display='none';
+}
