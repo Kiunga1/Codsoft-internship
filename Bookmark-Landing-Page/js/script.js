d5ab1002f5f2daa41bfbcf21cc210f416e97d403
ann5306 <annkiunga@gmail.com>
2024-03-05 08:34:36 +0300

    Add features section

--- Bookmark-Landing-Page/js/script.js
+++ Bookmark-Landing-Page/js/script.js
@@ -1,3 +1,4 @@
+//Function to toggle overlay visibility
 function showOverlay() {
     document.getElementById('overlay').style.display = 'flex';
     document.getElementById('overlay').classList.add('Slide-from-right');
@@ -5,4 +6,16 @@ function showOverlay() {
 
 function hideOverlay() {
     document.getElementById('overlay').style.display='none';
+}
+
+//function to toggle Feature items visibility
+function toggleFeature (featureId) {
+    let feature = document.getElementById(featureId);
+    if (feature.classList.contains('hidden')){
+        //show the feature
+        feature.classList.remove('hidden');
+    } else {
+        //Hide feature
+        feature.classList.add ('hidden');
+    }
 }
