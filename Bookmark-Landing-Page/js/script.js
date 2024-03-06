0442a161267ea60da93cb6ff89843343a54e6d83
ann5306 <annkiunga@gmail.com>
2024-03-06 08:24:43 +0300

    Modify toggleFeature function to display only one feature at a time

--- Bookmark-Landing-Page/js/script.js
+++ Bookmark-Landing-Page/js/script.js
@@ -9,13 +9,26 @@ function hideOverlay() {
 }
 
 //function to toggle Feature items visibility
-function toggleFeature (featureId) {
+const toggleFeature = (featureId) => {
     let feature = document.getElementById(featureId);
-    if (feature.classList.contains('hidden')){
-        //show the feature
-        feature.classList.remove('hidden');
-    } else {
-        //Hide feature
-        feature.classList.add ('hidden');
-    }
+    //get all feature elements and loop through them 
+    let allFeatures = document.querySelectorAll('.feature');
+    
+    allFeatures.forEach(item =>{ 
+        //check for feature that's not being toggled and hide the feature
+        if (item.id !== featureId) {
+            item.classList.add('hidden');
+        }
+    });
+    //Toggle visibility of selected feature
+    feature.classList.toggle('hidden');
 }
+
+// Function to show the first feature initially when the page loads
+const showFirstFeature = () => {
+    const firstFeatureId = document.querySelectorAll('.feature')[0].id;
+    toggleFeature(firstFeatureId);
+};
+
+// Event listener to call the function to show the first feature when the page loads
+document.addEventListener("DOMContentLoaded", showFirstFeature);
