f5a9605931c35eefcfc38637f771ef4cbbeee316
ann5306 <annkiunga@gmail.com>
2024-03-07 21:59:55 +0300

    Add contact section

--- Bookmark-Landing-Page/js/script.js
+++ Bookmark-Landing-Page/js/script.js
@@ -1,3 +1,4 @@
+//Header section
 //Function to toggle overlay visibility
 function showOverlay() {
     document.getElementById('overlay').style.display = 'flex';
@@ -8,6 +9,8 @@ function hideOverlay() {
     document.getElementById('overlay').style.display='none';
 }
 
+
+//Features section
 //function to toggle Feature items visibility
 const toggleFeature = (featureId) => {
     let feature = document.getElementById(featureId);
@@ -32,3 +35,15 @@ const showFirstFeature = () => {
 
 // Event listener to call the function to show the first feature when the page loads
 document.addEventListener("DOMContentLoaded", showFirstFeature);
+
+//More-info section
+//Get all questions and Add event listener to each  question
+const questionTitles = document.querySelectorAll('.question-title');
+
+questionTitles.forEach(title => {
+    title.addEventListener('click', () => {
+        //Toggle the displaying of the answer for the  clicked question which is the next element sibling
+        const answer = title.nextElementSibling;
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    })
+});
