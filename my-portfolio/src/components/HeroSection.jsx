bf0530ec5557d9773cd0f9d350127672075772e4
ann5306 <annkiunga@gmail.com>
2024-03-11 18:19:49 +0300

    Add styling to the <a>

--- my-portfolio/src/components/HeroSection.jsx
+++ my-portfolio/src/components/HeroSection.jsx
@@ -9,13 +9,13 @@ const HeroSection = () => {
         <div className="md:w-1/2 mb-8 md:mb-0">
           <img src={HeroImage} alt="Hero" className="w-full rounded-full" />
         </div>
-        <div className="md:w-1/2 md:pl-8">
-          <h2 className="text-3xl font-bold mb-4">Nice to meet you! I'm Ann Kiunga.</h2>
+        <div className="md:w-1/2 md:pl-8 text-center md:text-left">
+          <h2 className="text-3xl font-bold mb-6">Nice to meet you!<br/> I'm Ann Kiunga.</h2>
           <p className="text-lg mt-3">  I'm a front-end developer passionate about 
             building accessible web apps that users love.
             Let's collaborate to create standout digital solutions and build something beautiful together.</p>
-          <p className="mt-6 underline-custom">
-            <a href="#contact" className=" hover:text-blue-400 transition-colors font-semibold">CONTACT ME</a>
+          <p className="mt-6 ">
+            <a href="#contact" className=" hover:text-blue-400 border-b-2 border-blue-400 transition-colors font-semibold">CONTACT ME</a>
           </p>
         </div>
       </div>
