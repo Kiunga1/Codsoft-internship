6e5129877547b42848c2867bb0b0de85f0320df5
ann5306 <annkiunga@gmail.com>
2024-03-11 16:00:42 +0300

    Add skills section

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
+import SkillsSection from './components/SkillsSection';
 
 const App = () => {
   return (
-    <div className='min-h-screen'>
+    <div className='min-h-screen bg-gray-300'>
       <Header/>
       <HeroSection/>
+      <div className="container mx-auto px-4">
+        <hr className="section-divider" />
+      </div>
+      <SkillsSection/>
     </div>
   )
 }
