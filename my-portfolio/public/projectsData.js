1591f4b05f1fb5b3da2efa894f81511392d67b8a
ann5306 <annkiunga@gmail.com>
2024-03-24 12:51:42 +0300

    Few changes

--- my-portfolio/public/projectsData.js
+++ my-portfolio/public/projectsData.js
@@ -1,56 +1,56 @@
  //project data
  const projectsData = [
     {
-      imageUrl: 'public/assets/github-user-thumbnail.webp',
+      imageUrl: './assets/github-user-thumbnail.webp',
       title: 'Github User Search App',
       skills: ['React', 'Tailwind CSS', 'JavaScript'],
       projectUrl: 'https://github-user-search-app-liart-two.vercel.app/',
       codeUrl: 'https://github.com/Kiunga1/github_user_search_app'
     },
     {
-      imageUrl: 'public/assets/countries-api-project.webp',
+      imageUrl: './assets/countries-api-project.webp',
       title: 'World Countries API App',
       skills: ['React', 'Tailwind CSS', 'JavaScript'],
       projectUrl: 'https://worldinsightapp.netlify.app/',
       codeUrl: 'https://github.com/Kiunga1/Countries-App/tree/main'
     },
     {
-      imageUrl: 'public/assets/audiophile-ecommerce-project.webp',
+      imageUrl: './assets/audiophile-ecommerce-project.webp',
       title: 'Audiophile E-commerce Website',
       skills: ['React', 'CSS', 'JavaScript'],
       projectUrl: 'https://audiophile-ecommerce-website-iota-nine.vercel.app/',
       codeUrl: 'https://github.com/Kiunga1/audiophile-ecommerce-website'
     },
     {
-      imageUrl: 'public/assets/thumbnail-project-2-large.webp',
+      imageUrl: './assets/thumbnail-project-2-large.webp',
       title: 'E-learning landing page',
       skills: ['React', 'Tailwind CSS', 'JavaScript'],
       projectUrl: 'https://skilled-e-learning-landing-page-by-a.netlify.app/',
       codeUrl: 'https://github.com/Kiunga1/Skilled-E-learning-landing-page'
     },
     {
-      imageUrl: 'public/assets/thumbnail-project-3-large.webp',
+      imageUrl: './assets/thumbnail-project-3-large.webp',
       title: 'Todo web app',
       skills: ['React', 'JavaScript', 'Tailwind CSS'],
       projectUrl: 'https://todoproapplication.netlify.app/',
       codeUrl: 'https://github.com/Kiunga1/To-do-app'
     },
     {
-      imageUrl: 'public/assets/t-mart website.png',
+      imageUrl: './assets/t-mart website.png',
       title: 'Boookmark Landing Page',
       skills: ['HTML', 'CSS', 'JavaScript'],
       projectUrl: 'https://kiunga1.github.io/E-commerce-website/#',
       codeUrl: 'https://github.com/Kiunga1/E-commerce-website'
     },
     {
-      imageUrl: 'public/assets/azubi-landingpage.png',
+      imageUrl: './assets/azubi-landingpage.png',
       title: 'AzubiAfrica Landing Page',
       skills: ['HTML', 'CSS', 'JavaScript'],
       projectUrl: 'https://kiunga1.github.io/Azubi_Africa_Landing_Page/',
       codeUrl: 'https://github.com/Kiunga1/Azubi_Africa_Landing_Page'
     },
     {
-      imageUrl: 'public/assets/bookmark-landing-page.webp',
+      imageUrl: './assets/bookmark-landing-page.webp',
       title: 'Boookmark Landing Page',
       skills: ['HTML', 'CSS', 'JavaScript'],
       projectUrl: 'https://bookmark-landing-page-pearl-two.vercel.app/',
