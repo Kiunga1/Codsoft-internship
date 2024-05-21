b2ad0770108a1e840fbab86e67a16b11bf5293da
ann5306 <annkiunga@gmail.com>
2024-05-21 14:27:48 +0300

    Edit project data

--- my-portfolio/public/projectsData.js
+++ my-portfolio/public/projectsData.js
@@ -22,13 +22,6 @@
       codeUrl: 'https://github.com/Kiunga1/audiophile-ecommerce-website'
     },
     {
-      imageUrl: './assets/thumbnail-project-2-large.webp',
-      title: 'E-learning landing page',
-      skills: ['React', 'Tailwind CSS', 'JavaScript'],
-      projectUrl: 'https://skilled-e-learning-landing-page-by-a.netlify.app/',
-      codeUrl: 'https://github.com/Kiunga1/Skilled-E-learning-landing-page'
-    },
-    {
       imageUrl: './assets/thumbnail-project-3-large.webp',
       title: 'Todo web app',
       skills: ['React', 'JavaScript', 'Tailwind CSS'],
@@ -37,7 +30,7 @@
     },
     {
       imageUrl: './assets/t-mart website.png',
-      title: 'Boookmark Landing Page',
+      title: 'E-commerce website Page',
       skills: ['HTML', 'CSS', 'JavaScript'],
       projectUrl: 'https://kiunga1.github.io/E-commerce-website/#',
       codeUrl: 'https://github.com/Kiunga1/E-commerce-website'
@@ -49,14 +42,6 @@
       projectUrl: 'https://kiunga1.github.io/Azubi_Africa_Landing_Page/',
       codeUrl: 'https://github.com/Kiunga1/Azubi_Africa_Landing_Page'
     },
-    {
-      imageUrl: './assets/bookmark-landing-page.webp',
-      title: 'Boookmark Landing Page',
-      skills: ['HTML', 'CSS', 'JavaScript'],
-      projectUrl: 'https://bookmark-landing-page-pearl-two.vercel.app/',
-      codeUrl: 'https://github.com/Kiunga1/Codsoft-internship/tree/main/Bookmark-Landing-Page'
-    }
-    
   ];
 
   export default projectsData
